8993db670fcabe5e062893025910f83d95262bda
Anurag <akalavakunta@gmail.com>
2015-04-04 14:59:33 -0700

    removed glut, added glfw + glm

--- asdfgame/main.cpp
+++ asdfgame/main.cpp
@@ -1,25 +1,59 @@
 #include <GL/glew.h>
-#include <GL/glut.h>
+#include <GLFW/glfw3.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 
 
-int main(int argc, char **argv) {
-	
-	// init glut
-	glutInit(&argc, argv); // initialize GLUT
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH); // open an OpenGL context with double buffering, RGB colors, and depth buffering
-	glutInitWindowSize(400, 400); // set initial window size
-	glutCreateWindow("asdfgame"); // open window and set window title
-	
-	// init glew
-	GLenum err = glewInit();
-	if (err != GLEW_OK) {
-		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-		exit(1);
+GLFWwindow* window;
+
+int main(int argc, char *argv[]) {
+
+	// Initialise GLFW
+	if (!glfwInit()) {
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		return -1;
 	}
 
-	glutMainLoop();
+	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow(1024, 768, "asdfgame", NULL, NULL);
+	if (window == NULL){
+		fprintf(stderr, "Failed to open GLFW window.\n");
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	GLenum glewInitResult = glewInit();
+	if (glewInitResult != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n%s\n", glewGetErrorString(glewInitResult));
+		return -1;
+	}
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Dark blue background
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	do {
+		// TODO graphics stuff
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Check if the ESC key was pressed or the window was closed
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
 
 	return 0;
 }
